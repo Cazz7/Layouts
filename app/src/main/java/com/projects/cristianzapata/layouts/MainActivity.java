bb25c414ab99b0ac8cfb499a931a106ee06c8e42
Cazz7 <cazz182@gmail.com>
2017-05-14 22:17:06 -0500

    otro botón agregado

--- app/src/main/java/com/projects/cristianzapata/layouts/MainActivity.java
+++ app/src/main/java/com/projects/cristianzapata/layouts/MainActivity.java
@@ -5,24 +5,33 @@ import android.support.annotation.StringRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-//    TextView campoView;
+    TextView texto;
+    EditText edit;
+    Button boton, boton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        campoView = (TextView) findViewById(R.id.textView1);
+        texto = (TextView) findViewById(R.id.textView1);
+        edit  = (EditText) findViewById(R.id.editText1);
+        boton = (Button) findViewById(R.id.button1);
+        boton2 = (Button) findViewById(R.id.button2);
 
-//        campoView.setText(R.string.pruebaTextView2);
+        boton.setOnClickListener(this); // Sólo este elemento tendrá habilitado el boton
+        boton2.setOnClickListener(this);// Igual aquí
     }
 
     @Override
@@ -45,5 +54,19 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button1:
+                String textoIngresado;
+                textoIngresado = edit.getText().toString();
+                texto.setText(textoIngresado);
+                break;
+            case R.id.button2:
+                texto.setText("Usted presionó el botón 2");
+                break;
+        }
     }
 }
